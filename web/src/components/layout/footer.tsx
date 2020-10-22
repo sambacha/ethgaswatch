08cf187daa5d60e50c5a83a5bd91f63dbe47757a
sam <sam@freighttrust.com>
2020-10-22 01:24:08 -0700

    artifact cleanup

--- web/src/components/layout/footer.tsx
+++ web/src/components/layout/footer.tsx
@@ -12,14 +12,11 @@ export const Footer = () => {
         <small>
             <a href="https://twitter.com/wslyvh" className="text-muted">@wslyvh</a>
             <span className="ml-2 mr-2">|</span>
-<<<<<<< HEAD
             <a href="https://github.com/wslyvh/ethgaswatch" className="text-muted">GasWatch GitHub</a> <br>
       <a href="https://github.com/sambacha/gasevo" className="text-muted">GasEVO GitHub</a>
-=======
             <a href="https://github.com/wslyvh/ethgaswatch" className="text-muted">Github</a>
             <span className="ml-2 mr-2">|</span>
             <a href="/privacy" className="text-muted">Privacy</a>
->>>>>>> 08c7c11bc825e512d204f56b6536a08af0b4a3ae
         </small>
     </footer>
   );
